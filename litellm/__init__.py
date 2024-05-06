9e6924ab64926db7dbd1de21d1b66266f7d9998a
jinno <klvx01@gmail.com>
2024-05-07 08:46:27 +0900

    fix(litellm/__init__.py): add provider_by_model dict

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -557,6 +557,8 @@ models_by_provider: dict = {
     "watsonx": watsonx_models,
 }
 
+provider_by_model = {}  # This will initialized by get_provider_from_model()
+
 # mapping for those models which have larger equivalents
 longer_context_model_fallback_dict: dict = {
     # openai chat completion models
